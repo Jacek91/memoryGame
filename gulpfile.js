27687e23ac5a5e717c8a720a417988d7e7f59d70
Jacek91 <jaczaw91@gmail.com>
2019-01-30 21:16:34 +0100

    complete js file and add some changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,17 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require("gulp-sourcemaps");
 
-gulp.task('sass', function()
-{
+gulp.task('sass', function () {
   return gulp.src('scss/main.scss')
-  .pipe(sourcemaps.init())
-  .pipe(sass({
-          outputStyle:    'expanded',
-          sourceComments: 'map'
-  }))
-  .pipe(sourcemaps.write())
-  .pipe(gulp.dest('css'))
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      outputStyle: 'expanded',
+      sourceComments: 'map'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'))
 });
 
-gulp.task('watch', function()
-{
+gulp.task('watch', function () {
   gulp.watch('scss/**/*.scss', ['sass']);
 });
--- js/app.js
+++ js/app.js
@@ -17,13 +17,18 @@ $(function () {
         "./images/tyrion.jpg",
         "./images/tyrion.jpg"
     ];
-    
     const game = $(".game");
     const movesNum = $(".movesCounter");
     const timeNum = $(".timeCounter");
+    const restart = $(".restart");
+    const start = $(".start");
+    const timeCounter = $(".timeCounter");
     let counter = 30;
+    let gameStart = false;
+    let matches = 0;
     let moves = 0;
-    
+    let opened = [];
+    let flag = true;
 
     shuffle = array => {
         for (let i = array.length - 1; i > 0; i--) {
@@ -54,5 +59,100 @@ $(function () {
             );
         }
     };
-    newGame()
+
+    game.on("click", '.card:not(".match, .flip")', function () {
+        if (game.find(".flip").length >= 2 || !gameStart) {
+            return false;
+        }
+
+        let flipped = $(this).data("elem");
+        $(this).addClass("flip");
+        opened.push(flipped);
+
+        if (opened.length > 1) {
+            if (opened[0] === opened[1]) {
+                game.find(".flip").addClass("match");
+                setTimeout(() => {
+                    game.find(".match").removeClass("flip");
+                }, 500);
+                matches++;
+            } else {
+                game.find(".flip").addClass("notmatch");
+                setTimeout(() => {
+                    game.find(".flip").removeClass("flip notmatch");
+                }, 500);
+            }
+            opened = [];
+            moves++;
+            movesNum.html(moves);
+        }
+        if (matches === 8) {
+            setTimeout(function () {
+                endGame(moves);
+            }, 500);
+        }
+    });
+
+    reset = () => {
+        swal({
+            allowEscapeKey: false,
+            allowOutsideClick: false,
+            title: "Are you sure?",
+            text: "Your progress will be lost!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#8e793e",
+            cancelButtonColor: "#ad974f",
+            confirmButtonText: "Yes, Restart Game!"
+        }).then(function (isConfirm) {
+            if (isConfirm) {
+                flag = false;
+                newGame();
+                timer();
+            }
+        }).catch(swal.noop);
+    };
+
+    endGame = moves => {
+        swal({
+            allowEscapeKey: false,
+            allowOutsideClick: false,
+            title: "Congratulations! You Won!",
+            text: "With " + moves + " moves!",
+            type: "success",
+            confirmButtonColor: "#8e793e",
+            confirmButtonText: "Play again!"
+        }).then(function (isConfirm) {
+            if (isConfirm) {
+                flag = false;
+                newGame();
+                timer();
+            }
+        });
+    };
+
+    timer = () => {
+        gameStart = true;
+        let counter = 30;
+        var int = setInterval(() => {
+            if (matches === 8 || counter === 0) {
+                game.find(".flip").removeClass("flip notmatch");
+                gameStart = false;
+                clearInterval(int);
+                return false;
+            } else if (!flag) {
+                clearInterval(int);
+                flag = true;
+            } else {
+                counter--;
+                timeCounter.text(counter + " s");
+            }
+        }, 1000);
+        newGame();
+    };
+
+    start.on("click", timer);
+    restart.on("click", reset);
+
+    newGame();
 });
